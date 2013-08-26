3865f2d1129ea875b1c3dce032ef22fb076ad763
hippyPig <samcphall@gmail.com>
2013-08-27 00:27:30 +0200

    Changes for v2

--- wol2.py
+++ wol2.py
@@ -1,7 +1,41 @@
 ################################################################################
 import sys
 import os
+import urllib
 ################################################################################
+def get_arxiv_details(arxiv):
+    url = wolvars.get_arxiv_abs(arxiv)
+    data = urllib.urlopen(url).read()
+    parser = LitHTMLParser()
+    parser.feed(data)
+    urlname = wolvars.get_arxiv_pdf(parser.filename)
+    if not os.path.exists('%s/%s' % (wolvars.arxiv_dir, parser.filename)):
+        cmd = 'wget -q --user-agent=Lynx %s -O %s/%s.pdf'
+        cmd = cmd % (urlname, wolvars.arxiv_dir, parser.filename)
+        os.system(cmd)
+    return parser.filename, parser.title
+
+
+def add(args):
+    move_dir = args.pop(-1)
+    arxivs = [str2arxiv(x) for x in args]
+    test_dir = arxiv_format(move_dir)
+    if not is_arxiv(test_dir):
+        directory = test_dir
+    else:
+        directory = 'papers'
+        arxiv.append(test_dir)
+    details = get_dot_wol()
+    for arxiv in arxivs:
+        qq
+
+    print 'add'
+
+def mv(args):
+    print 'mv'
+
+def update(args):
+    print 'update'
 
 def opts(args):
     if len(args) == 1:
@@ -10,10 +44,13 @@ def opts(args):
     operation = args.pop(0)
     if operation == 'add':
         print 'add'
+        add(args)
     elif operation == 'mv':
-        print 'add'
+        print 'mv'
+        mv(args)
     elif operation == 'update':
-        print 'add'
+        print 'update'
+        update(args)
     elif operation == 'recent':
         print 'add'
     return
--- wolClasses.py
+++ wolClasses.py
@@ -1,3 +1,28 @@
+from HTMLParser import HTMLParser
+
+class WolVars:
+    """Keeps all variables and pathnames and urls etc."""
+    arxiv_abs = 'http://arxiv.org/abs/%s'
+    arxiv_pdf = 'http://arxiv.org/pdf/%s.pdf'
+    lhcb_ana = 'https://cds.cern.ch/record/1384141/files/%s'
+    woldir = ''
+    arxiv_dir = ''
+    woldot = ''
+    def __init__(self):
+        self.woldir = os.getenv('WOLDIR')
+        if not self.woldir:
+            self.woldir = os.getcwd()
+        self.arxiv_dir = '%s/arxiv' % self.woldir
+        self.woldot = '%s/.wol' % self.woldir
+    #
+    def get_arxiv_abs(self, number):
+        return self.arxiv_abs % number
+    #
+    def get_arxiv_pdf(self, number):
+        return self.arxiv_pdf % number
+    #
+    def get_lhcb_ana(self, name):
+        return self.lhcb_ana % name
 
 
 class WolDictHandler:
@@ -17,12 +42,15 @@ class WolDictHandler:
         return
     #
     def add(self, arxiv, vers, directory, title):
-        try:
-            self.details[arxiv] = {'version' : vers,
-                                   'dir' : directory,
-                                   'title' : title}
-        except KeyError:
-            raise
+        title = title.replace('\n', '')
+        title = title.replace('$', '')
+        title = title.replace('  ', ' ')
+        title = title.replace('/', '')
+        title = title.replace('--gt;', '\\to')
+        title = title.replace('-gt;', '\\to')
+        self.details[arxiv] = {'version' : vers,
+                               'dir' : directory,
+                               'title' : title}
         return
     #
     def remove(self, arxiv):
@@ -39,3 +67,29 @@ class WolDictHandler:
         self.details[arxiv]['dir'] = directory
         return True
  
+
+class LitHTMLParser(HTMLParser):
+    """Parses HTML and keeps title and filename."""
+    title = ''
+    filename = ''
+    get_title = False
+    get_filename = False
+    def handle_starttag(self, tag, attrs):
+        if tag == 'title':
+            self.get_title = True
+        if tag == 'a':
+            self.get_filename = True
+        #print 'Start', tag
+    def handle_endtag(self, tag):
+        if tag == 'title':
+            self.get_title = False
+        if tag == 'a':
+            self.get_filename = False
+        #print 'End', tag
+    def handle_data(self, data):
+        if self.get_title:
+            self.title += data
+        elif self.get_filename:
+            if data.startswith('arXiv:'):
+                self.filename = data.replace('arXiv:', '')
+        #print 'Data', data
