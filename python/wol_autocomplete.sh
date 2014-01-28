0d8091b006c34cd5c11f30dfe42899bdcfc251ab
hippyPig <samcphall@gmail.com>
2014-01-28 23:58:49 +0100

    Addition of autocompletion

---
+++ python/wol_autocomplete.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+_wol()
+{
+  cur=${COMP_WORDS[COMP_CWORD]}
+  case "${cur}" in
+    a*) use="add" ;;
+    addw*) use="addwol " ;;
+    m*) use="move " ;;
+    f*) use="find " ;;
+    s*) use="show " ;;
+    c*) use="config " ;;
+    u*) use="update " ;;
+    d*) use="del " ;;
+    i*) use="info " ;;
+    b*) use="browse " ;;
+  esac
+  COMPREPLY=( $( compgen -W "$use" -- $cur ) )
+}
+complete -o default -F _wol  wol
+#complete -o default -o nospace -F _wol  wol
